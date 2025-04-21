f8418afc687e495a6658a634dd32bb533659dcab
nineztech <nineztechllc@gmail.com>
2025-04-21 17:31:15 +0530

    Delete src directory

--- src/types/project.ts
+++
@@ -1,9 +0,0 @@
-export interface Project {
-  id: string;
-  name: string;
-  location: string;
-  capacity: string;
-  category: 'commercial' | 'industrial' | 'residential' | 'ground-mounted' | 'solar-pump';
-  image: string;
-  description?: string;
-}
